11e5219354582287919a183bda3306698cbfdde1
Simon Ogorodnik <Simon.Ogorodnik@jetbrains.com>
2018-03-14 18:44:41 +0300

    Make visitor generator insert copyright

--- compiler/fir/tree/visitors-generator/build.gradle.kts
+++ compiler/fir/tree/visitors-generator/build.gradle.kts
@@ -1,3 +1,5 @@
+import tasks.WriteCopyrightToFile
+
 /*
  * Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license
  * that can be found in the license/LICENSE.txt file.
@@ -18,10 +20,18 @@ dependencies {
     compile(intellijDep()) { includeJars("guava", rootProject = rootProject) }
 }
 
+val writeCopyright by task<WriteCopyrightToFile> {
+    outputFile = file("$buildDir/copyright/notice.txt")
+    commented = true
+}
+
+val processResources by tasks
+processResources.dependsOn(writeCopyright)
 
 sourceSets {
     "main" {
         projectDefault()
+        resources.srcDir("$buildDir/copyright")
     }
     "test" {}
 }
