688070a54196a4462be3dd22a8e8b56a41fa5aa9
Mikhail Glukhikh <Mikhail.Glukhikh@jetbrains.com>
2018-03-15 15:22:55 +0300

    FIR: fix ErrorTypeImpl base type

--- compiler/fir/tree/src/org/jetbrains/kotlin/fir/types/impl/FirErrorTypeImpl.kt
+++ compiler/fir/tree/src/org/jetbrains/kotlin/fir/types/impl/FirErrorTypeImpl.kt
@@ -7,10 +7,15 @@ package org.jetbrains.kotlin.fir.types.impl
 
 import com.intellij.psi.PsiElement
 import org.jetbrains.kotlin.fir.FirSession
-import org.jetbrains.kotlin.fir.types.FirDynamicType
+import org.jetbrains.kotlin.fir.types.FirErrorType
+import org.jetbrains.kotlin.fir.visitors.FirVisitor
 
 class FirErrorTypeImpl(
     session: FirSession,
     psi: PsiElement?,
     isNullable: Boolean
-) : FirAbstractAnnotatedType(session, psi, isNullable), FirDynamicType
+) : FirAbstractAnnotatedType(session, psi, isNullable), FirErrorType {
+    override fun <R, D> accept(visitor: FirVisitor<R, D>, data: D): R {
+        return super<FirErrorType>.accept(visitor, data)
+    }
+}
