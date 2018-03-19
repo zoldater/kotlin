00625b3e443ef39c2e8f01b2cbbccfb6173a7d04
Mikhail Glukhikh <Mikhail.Glukhikh@jetbrains.com>
2018-03-19 17:58:42 +0300

    FIR: support qualified types

--- compiler/fir/tree/src/org/jetbrains/kotlin/fir/types/impl/FirUserTypeImpl.kt
+++ compiler/fir/tree/src/org/jetbrains/kotlin/fir/types/impl/FirUserTypeImpl.kt
@@ -7,15 +7,14 @@ package org.jetbrains.kotlin.fir.types.impl
 
 import com.intellij.psi.PsiElement
 import org.jetbrains.kotlin.fir.FirSession
+import org.jetbrains.kotlin.fir.types.FirQualifier
 import org.jetbrains.kotlin.fir.types.FirUserType
-import org.jetbrains.kotlin.fir.types.FirTypeProjection
-import org.jetbrains.kotlin.name.Name
+import java.util.*
 
 class FirUserTypeImpl(
     session: FirSession,
     psi: PsiElement?,
-    isNullable: Boolean,
-    override val name: Name
+    isNullable: Boolean
 ) : FirAbstractAnnotatedType(session, psi, isNullable), FirUserType {
-    override val typeArguments = mutableListOf<FirTypeProjection>()
+    override val qualifiers = LinkedList<FirQualifier>()
 }
