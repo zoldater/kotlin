9cc2fb2dadf19beb81437a28990766ddb5044773
Simon Ogorodnik <simon.ogorodnik@jetbrains.com>
2018-03-20 01:17:40 +0300

    FIR: Add module for new types aka 'cones' (of fir tree)

--- build.gradle.kts
+++ build.gradle.kts
@@ -187,6 +187,8 @@ extra["compilerModules"] = arrayOf(
         ":compiler:fir",
         ":compiler:fir:tree",
         ":compiler:fir:psi2fir",
+        ":compiler:fir:resolve",
+        ":compiler:fir:cones",
         ":compiler:frontend",
         ":compiler:frontend.java",
         ":compiler:frontend.script",
