5a9358e5d6e5b549e7f9a7c1e6ed677d920470d5
Simon Ogorodnik <Simon.Ogorodnik@jetbrains.com>
2018-03-26 22:21:51 +0200

    FIR: Hack-in java model

---
+++ compiler/fir/java/build.gradle.kts
@@ -0,0 +1,22 @@
+
+apply { plugin("kotlin") }
+apply { plugin("jps-compatible") }
+
+jvmTarget = "1.6"
+
+dependencies {
+    val compile by configurations
+    val compileOnly by configurations
+
+
+    compile(project(":compiler:frontend.java"))
+    compile(project(":compiler:fir:resolve"))
+
+    compileOnly(intellijCoreDep()) { includeJars("intellij-core", "annotations") }
+}
+
+
+sourceSets {
+    "main" { projectDefault() }
+    "test" {}
+}
---
+++ compiler/fir/java/src/org/jetbrains/kotlin/fir/java/TypeConverter.kt
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2010-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license
+ * that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.fir.java
+
+import org.jetbrains.kotlin.fir.FirSession
+import org.jetbrains.kotlin.fir.types.ConeClassLikeType
+import org.jetbrains.kotlin.fir.types.ConeKotlinType
+import org.jetbrains.kotlin.load.java.structure.JavaClass
+import org.jetbrains.kotlin.load.java.structure.JavaClassifierType
+import org.jetbrains.kotlin.load.java.structure.JavaType
+
+class TypeConverter(val session: FirSession) {
+
+//    fun convertType(type: JavaType): ConeKotlinType? {
+//
+//    }
+//
+//    fun convertClassLikeType(type: JavaClassifierType): ConeClassLikeType? {
+//        //val classifier = type.classifier as? JavaClass
+//
+//    }
+}
