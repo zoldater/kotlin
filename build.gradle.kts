8d9a40a2355aa1567c1ebba5f12b60bbb9ced70b
Simon Ogorodnik <Simon.Ogorodnik@jetbrains.com>
2018-04-05 15:53:04 +0300

    Extract module info & target platform to separate module

--- build.gradle.kts
+++ build.gradle.kts
@@ -181,6 +181,7 @@ extra["compilerModules"] = arrayOf(
         ":compiler:resolution",
         ":compiler:serialization",
         ":compiler:psi",
+        ":compiler:modules",
         ":compiler:fir",
         ":compiler:fir:tree",
         ":compiler:fir:psi2fir",
