0bca84330d3dcf73d043a894ebe564be8fe4b8cf
Simon Ogorodnik <simon.ogorodnik@jetbrains.com>
2018-04-12 01:34:08 +0300

    FIR: Fix error type in java supertype resolution

--- compiler/fir/java/src/org/jetbrains/kotlin/fir/java/JavaSymbolProviders.kt
+++ compiler/fir/java/src/org/jetbrains/kotlin/fir/java/JavaSymbolProviders.kt
@@ -9,7 +9,10 @@ import com.intellij.openapi.project.Project
 import com.intellij.psi.search.GlobalSearchScope
 import org.jetbrains.kotlin.fir.java.symbols.JavaClassSymbol
 import org.jetbrains.kotlin.fir.resolve.AbstractFirSymbolProvider
+import org.jetbrains.kotlin.fir.symbols.ConeClassLikeSymbol
 import org.jetbrains.kotlin.fir.symbols.ConeSymbol
+import org.jetbrains.kotlin.load.java.structure.JavaClass
+import org.jetbrains.kotlin.load.java.structure.classId
 import org.jetbrains.kotlin.name.ClassId
 import org.jetbrains.kotlin.name.FqName
 import org.jetbrains.kotlin.resolve.jvm.KotlinJavaPsiFacade
@@ -23,7 +26,14 @@ class JavaSymbolProvider(
         return classCache.lookupCacheOrCalculate(classId) {
             val facade = KotlinJavaPsiFacade.getInstance(project)
             val foundClass = facade.findClass(classId, searchScope)
-            foundClass?.let { JavaClassSymbol(it) }
+            foundClass?.let { JavaClassSymbol(this, it) }
+        }
+    }
+
+    fun getSymbolByJavaClass(javaClass: JavaClass): ConeSymbol? {
+        val classId = javaClass.classId ?: error("!")
+        return classCache.lookupCacheOrCalculate(classId) {
+            javaClass.let { JavaClassSymbol(this, it) }
         }
     }
 
