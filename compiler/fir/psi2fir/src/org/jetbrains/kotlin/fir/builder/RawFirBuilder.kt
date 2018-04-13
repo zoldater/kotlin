c1cd008647dfb847e61a3a8c1b7b298fd9b759b7
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2018-04-13 11:15:34 +0300

    FIR: add implicit primary constructors, add delegated types to them

--- compiler/fir/psi2fir/src/org/jetbrains/kotlin/fir/builder/RawFirBuilder.kt
+++ compiler/fir/psi2fir/src/org/jetbrains/kotlin/fir/builder/RawFirBuilder.kt
@@ -38,6 +38,10 @@ class RawFirBuilder(val session: FirSession) {
 
     private val implicitUnitType = FirImplicitUnitType(session, null)
 
+    private val implicitAnyType = FirImplicitAnyType(session, null)
+
+    private val implicitEnumType = FirImplicitEnumType(session, null)
+
     fun buildFirFile(file: KtFile): FirFile {
         return file.accept(Visitor(), Unit) as FirFile
     }
@@ -205,13 +209,15 @@ class RawFirBuilder(val session: FirSession) {
 
         private fun KtClassOrObject.extractSuperTypeListEntriesTo(container: FirClassImpl) {
             var superTypeCallEntry: KtSuperTypeCallEntry? = null
+            var delegatedSuperType: FirType? = null
             for (superTypeListEntry in superTypeListEntries) {
                 when (superTypeListEntry) {
                     is KtSuperTypeEntry -> {
                         container.superTypes += superTypeListEntry.typeReference.toFirOrErrorType()
                     }
                     is KtSuperTypeCallEntry -> {
-                        container.superTypes += superTypeListEntry.calleeExpression.typeReference.toFirOrErrorType()
+                        delegatedSuperType = superTypeListEntry.calleeExpression.typeReference.toFirOrErrorType()
+                        container.superTypes += delegatedSuperType
                         superTypeCallEntry = superTypeListEntry
                     }
                     is KtDelegatedSuperTypeEntry -> {
@@ -223,31 +229,40 @@ class RawFirBuilder(val session: FirSession) {
                     }
                 }
             }
-            val firPrimaryConstructor = primaryConstructor?.toFirConstructor(superTypeCallEntry) ?: return
+            fun isEnum() = this is KtClass && this.isEnum()
+            if (this is KtClass && this.isInterface()) return
+            if (!this.hasPrimaryConstructor()) return
+            val firPrimaryConstructor = primaryConstructor.toFirConstructor(
+                superTypeCallEntry,
+                delegatedSuperType = delegatedSuperType ?: (if (isEnum()) implicitEnumType else implicitAnyType),
+                owner = this
+            )
             container.declarations += firPrimaryConstructor
         }
 
-        private fun KtPrimaryConstructor.toFirConstructor(superTypeCallEntry: KtSuperTypeCallEntry?): FirConstructor {
+        private fun KtPrimaryConstructor?.toFirConstructor(
+            superTypeCallEntry: KtSuperTypeCallEntry?,
+            delegatedSuperType: FirType,
+            owner: KtClassOrObject
+        ): FirConstructor {
             val constructorCallee = superTypeCallEntry?.calleeExpression
-            val firDelegatedCall = constructorCallee?.let {
-                FirDelegatedConstructorCallImpl(
-                    session,
-                    constructorCallee,
-                    FirErrorTypeImpl(session, constructorCallee, "Not implemented yet"),
-                    isThis = false
-                ).apply {
-                    // TODO: arguments are not needed for light classes, but will be needed later
-                    //superTypeCallEntry.extractArgumentsTo(this)
-                }
+            val firDelegatedCall = FirDelegatedConstructorCallImpl(
+                session,
+                constructorCallee ?: (this ?: owner),
+                delegatedSuperType,
+                isThis = false
+            ).apply {
+                // TODO: arguments are not needed for light classes, but will be needed later
+                //superTypeCallEntry.extractArgumentsTo(this)
             }
             val firConstructor = FirPrimaryConstructorImpl(
                 session,
-                this,
-                visibility,
+                this ?: owner,
+                this?.visibility ?: Visibilities.UNKNOWN,
                 firDelegatedCall
             )
-            extractAnnotationsTo(firConstructor)
-            extractValueParametersTo(firConstructor)
+            this?.extractAnnotationsTo(firConstructor)
+            this?.extractValueParametersTo(firConstructor)
             return firConstructor
         }
 
