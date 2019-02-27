449b3b3813d1a247061724669315cfd2782a5b5c
Dmitry Savvinov <dmitry.savvinov@jetbrains.com>
2019-02-27 11:52:35 +0300

    [Resolve] Remove platform from PlatformAnalysisSettings (effectively removes separate universes for different platforms)

--- idea/idea-analysis/src/org/jetbrains/kotlin/idea/caches/resolve/ProjectResolutionFacade.kt
+++ idea/idea-analysis/src/org/jetbrains/kotlin/idea/caches/resolve/ProjectResolutionFacade.kt
@@ -138,7 +138,7 @@ internal class ProjectResolutionFacade(
             modulesContentFactory,
             moduleLanguageSettingsProvider = IDELanguageSettingsProvider,
             resolverForModuleFactoryByPlatform = { modulePlatform ->
-                val platform = modulePlatform ?: settings.platform
+                val platform = modulePlatform ?: TODO()
                 platform.idePlatformKind.resolution.resolverForModuleFactory
             },
             platformParameters = { platform ->
@@ -212,7 +212,9 @@ internal class ProjectResolutionFacade(
 
     private companion object {
         private fun createBuiltIns(settings: PlatformAnalysisSettings, projectContext: ProjectContext): KotlinBuiltIns {
-            return settings.platform.idePlatformKind.resolution.createBuiltIns(settings, projectContext)
+//            return settings.platform.idePlatformKind.resolution.createBuiltIns(settings, projectContext)
+//             TODO: what we do about built-ins?
+            return DefaultBuiltInPlatforms.jvmPlatform.idePlatformKind.resolution.createBuiltIns(settings, projectContext)
         }
     }
 }
