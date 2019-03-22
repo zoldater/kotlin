42e37860e125d4c54d7e4e567cd996ce3c220a9b
Nicolay Mitropolsky <nicolay.mitropolsky@jetbrains.com>
2019-03-22 12:12:25 +0300

    ~~~~ switch 192 ~~~~

--- idea/tests/org/jetbrains/kotlin/idea/quickfix/CommonIntentionActionsParametersTest.kt
+++ idea/tests/org/jetbrains/kotlin/idea/quickfix/CommonIntentionActionsParametersTest.kt
@@ -16,6 +16,11 @@ class CommonIntentionActionsParametersTest : LightPlatformCodeInsightFixtureTest
 
     override fun getProjectDescriptor() = KotlinWithJdkAndRuntimeLightProjectDescriptor.INSTANCE_FULL_JDK
 
+    override fun setUp() {
+        super.setUp()
+        myFixture.configureByText("Anno.kt", "annotation class Anno(val i: Int)")
+    }
+
     fun testSetParameters() {
         myFixture.configureByText(
             "foo.kt",
@@ -49,5 +54,116 @@ class CommonIntentionActionsParametersTest : LightPlatformCodeInsightFixtureTest
         )
     }
 
+
+    fun testAddParameterToTheEnd() {
+        myFixture.configureByText(
+            "foo.kt",
+            """
+            class Foo {
+                fun ba<caret>r(@Anno(3) a: Int) {}
+            }
+            """.trimIndent()
+        )
+
+        runParametersTransformation("Change method parameters to '(a: Int, file: File)'") { currentParameters ->
+            currentParameters + expectedParameter(
+                PsiType.getTypeByName("java.io.File", project, myFixture.file.resolveScope), "file",
+                listOf(annotationRequest("Anno", intAttribute("i", 8)))
+            )
+        }
+
+        myFixture.checkResult(
+            """
+            import java.io.File
+
+            class Foo {
+                fun bar(@Anno(3) a: Int, @Anno(i = 8) file: File) {}
+            }
+            """.trimIndent(), true
+        )
+    }
+
+
+    fun testAddParameterToTheBeginning() {
+        myFixture.configureByText(
+            "foo.kt",
+            """
+            class Foo {
+                fun ba<caret>r(@Anno(3) a: Int) {}
+            }
+            """.trimIndent()
+        )
+
+        runParametersTransformation("Change method parameters to '(file: File, a: Int)'") { currentParameters ->
+            listOf(
+                expectedParameter(
+                    PsiType.getTypeByName("java.io.File", project, myFixture.file.resolveScope), "file",
+                    listOf(annotationRequest("Anno", intAttribute("i", 8)))
+                )
+            ) + currentParameters
+        }
+
+        myFixture.checkResult(
+            """
+            import java.io.File
+
+            class Foo {
+                fun bar(@Anno(i = 8) file: File, @Anno(3) a: Int) {}
+            }
+            """.trimIndent(), true
+        )
+    }
+
+    fun testReplaceInTheMiddle() {
+        myFixture.configureByText(
+            "foo.kt",
+            """
+            class Foo {
+                fun ba<caret>r(@Anno(1) a: Int, @Anno(2) b: Int, @Anno(3) c: Int) {}
+            }
+            """.trimIndent()
+        )
+
+        runParametersTransformation("Change method parameters to '(a: Int, file: File, c: Int)'") { currentParameters ->
+            ArrayList<ExpectedParameter>(currentParameters).apply {
+                this[1] = expectedParameter(
+                    PsiType.getTypeByName("java.io.File", project, myFixture.file.resolveScope), "file",
+                    listOf(annotationRequest("Anno", intAttribute("i", 8)))
+                )
+            }
+        }
+
+        myFixture.checkResult(
+            """
+            import java.io.File
+
+            class Foo {
+                fun bar(@Anno(1) a: Int, @Anno(i = 8) file: File, @Anno(3) c: Int) {}
+            }
+            """.trimIndent(), true
+        )
+    }
+
+    private fun runParametersTransformation(
+        actionName: String,
+        transformation: (List<ChangeParametersRequest.ExistingParameterWrapper>) -> List<ExpectedParameter>
+    ) {
+        val psiMethod = myFixture.atCaret<UMethod>().javaPsi
+        val currentParameters = psiMethod.parameters.map { ChangeParametersRequest.ExistingParameterWrapper(it) }
+        myFixture.launchAction(
+            createChangeParametersActions(
+                psiMethod,
+                SimpleChangeParametersRequest(
+                    transformation(currentParameters)
+                )
+            ).findWithText(actionName)
+        )
+    }
+
 }
 
+private class SimpleChangeParametersRequest(private val list: List<ExpectedParameter>) : ChangeParametersRequest {
+    override fun getExpectedParameters(): List<ExpectedParameter> = list
+
+    override fun isValid(): Boolean = true
+}
