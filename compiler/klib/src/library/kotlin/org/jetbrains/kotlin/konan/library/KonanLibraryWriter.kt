6cd8dc7535f80da077d03fc6a2cc4aa8f6fa51d8
Svyatoslav Kuzmich <svyatoslav.kuzmich@jetbrains.com>
2019-05-16 17:24:48 +0300

    Add KLIB writer sources

---
+++ compiler/klib/src/library/kotlin/org/jetbrains/kotlin/konan/library/KonanLibraryWriter.kt
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2010-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license
+ * that can be found in the LICENSE file.
+ */
+
+package org.jetbrains.kotlin.backend.konan.library
+
+//import llvm.LLVMModuleRef
+//import org.jetbrains.kotlin.backend.common.serialization.SerializedIr
+import org.jetbrains.kotlin.konan.library.KonanLibrary
+import org.jetbrains.kotlin.konan.library.KonanLibraryVersioning
+import org.jetbrains.kotlin.konan.properties.Properties
+
+interface KonanLibraryWriter {
+    val versions: KonanLibraryVersioning
+    fun addLinkData(linkData: LinkData)
+    fun addNativeBitcode(library: String)
+    fun addIncludedBinary(library: String)
+//    fun addKotlinBitcode(llvmModule: LLVMModuleRef)
+    fun addLinkDependencies(libraries: List<KonanLibrary>)
+    fun addManifestAddend(properties: Properties)
+    fun addDataFlowGraph(dataFlowGraph: ByteArray)
+    val mainBitcodeFileName: String
+    fun commit()
+}
+
+class LinkData(
+    val module: ByteArray,
+    val fragments: List<List<ByteArray>>,
+    val fragmentNames: List<String>,
+    val ir: SerializedIr? = null
+)
+
+class SerializedIr (
+    val module: ByteArray,
+    val combinedDeclarationFilePath: String
+)
+
+interface MetadataWriter {
+    fun addLinkData(linkData: LinkData)
+}
