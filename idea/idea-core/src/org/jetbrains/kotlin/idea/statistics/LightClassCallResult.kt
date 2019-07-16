7ca72a6f761977f199a8a9be37926d556bf4944f
Anton Yalyshev <anton.yalyshev@jetbrains.com>
2019-07-16 12:26:06 +0300

    Register statistics collector for LightClasses

---
+++ idea/idea-core/src/org/jetbrains/kotlin/idea/statistics/LightClassCallResult.kt
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2010-2019 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.idea.statistics
+
+enum class LightClassCallResult {
+    Success, FailReason1, FailReason2
+}
+
+fun logLightClassCallStatistics(result: LightClassCallResult) {
+    val data = mapOf("result" to result.toString())
+    KotlinFUSLogger.log(FUSEventGroups.Editor, "LightClassCall", data)
+}
