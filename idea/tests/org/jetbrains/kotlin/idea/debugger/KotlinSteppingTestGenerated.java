e1930c4c4f9f0b0840a3a42215b4112c23e8167b
Yan Zhulanow <yan.zhulanow@jetbrains.com>
2019-08-08 21:39:07 +0900

    Debugger: Fix "Step over" with inline function calls in non-functions (KT-20342)

--- idea/tests/org/jetbrains/kotlin/idea/debugger/KotlinSteppingTestGenerated.java
+++ idea/tests/org/jetbrains/kotlin/idea/debugger/KotlinSteppingTestGenerated.java
@@ -423,6 +423,31 @@ public class KotlinSteppingTestGenerated extends AbstractKotlinSteppingTest {
             runTest("idea/testData/debugger/tinyApp/src/stepping/stepOver/inlineCallInForRangeExpression.kt");
         }
 
+        @TestMetadata("inlineFunInClassInitializer.kt")
+        public void testInlineFunInClassInitializer() throws Exception {
+            runTest("idea/testData/debugger/tinyApp/src/stepping/stepOver/inlineFunInClassInitializer.kt");
+        }
+
+        @TestMetadata("inlineFunInClassInitializer2.kt")
+        public void testInlineFunInClassInitializer2() throws Exception {
+            runTest("idea/testData/debugger/tinyApp/src/stepping/stepOver/inlineFunInClassInitializer2.kt");
+        }
+
+        @TestMetadata("inlineFunInConstructor.kt")
+        public void testInlineFunInConstructor() throws Exception {
+            runTest("idea/testData/debugger/tinyApp/src/stepping/stepOver/inlineFunInConstructor.kt");
+        }
+
+        @TestMetadata("inlineFunInLazyProperty.kt")
+        public void testInlineFunInLazyProperty() throws Exception {
+            runTest("idea/testData/debugger/tinyApp/src/stepping/stepOver/inlineFunInLazyProperty.kt");
+        }
+
+        @TestMetadata("inlineFunInPropertyGetter.kt")
+        public void testInlineFunInPropertyGetter() throws Exception {
+            runTest("idea/testData/debugger/tinyApp/src/stepping/stepOver/inlineFunInPropertyGetter.kt");
+        }
+
         @TestMetadata("inlineFunctionSameLines.kt")
         public void testInlineFunctionSameLines() throws Exception {
             runTest("idea/testData/debugger/tinyApp/src/stepping/stepOver/inlineFunctionSameLines.kt");
