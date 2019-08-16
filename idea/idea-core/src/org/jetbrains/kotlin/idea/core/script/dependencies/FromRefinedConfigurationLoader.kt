c0a25f3e7c8fcfb5944f368975f214c9dfb7d11c
Sergey Rostov <sergey.rostov@jetbrains.com>
2019-08-16 17:41:12 +0300

    scripts: background loader

--- idea/idea-core/src/org/jetbrains/kotlin/idea/core/script/dependencies/FromRefinedConfigurationLoader.kt
+++ idea/idea-core/src/org/jetbrains/kotlin/idea/core/script/dependencies/FromRefinedConfigurationLoader.kt
@@ -5,15 +5,15 @@
 
 package org.jetbrains.kotlin.idea.core.script.dependencies
 
-import com.intellij.openapi.progress.EmptyProgressIndicator
 import com.intellij.openapi.progress.ProgressIndicator
+import com.intellij.openapi.progress.Task
 import com.intellij.openapi.progress.util.BackgroundTaskUtil
 import com.intellij.openapi.project.Project
+import com.intellij.util.Alarm
+import com.intellij.util.containers.HashSetQueue
 import org.jetbrains.kotlin.idea.core.script.ScriptClassRootsManager
 import org.jetbrains.kotlin.idea.core.script.ScriptConfigurationManagerImpl
-import org.jetbrains.kotlin.idea.core.script.ScriptDependenciesManager
 import org.jetbrains.kotlin.idea.core.script.debug
-import org.jetbrains.kotlin.idea.core.script.settings.KotlinScriptingSettings
 import org.jetbrains.kotlin.psi.KtFile
 import org.jetbrains.kotlin.scripting.definitions.KotlinScriptDefinition
 import org.jetbrains.kotlin.scripting.definitions.ScriptDefinition
@@ -21,10 +21,9 @@ import org.jetbrains.kotlin.scripting.definitions.findScriptDefinition
 import org.jetbrains.kotlin.scripting.resolve.KtFileScriptSource
 import org.jetbrains.kotlin.scripting.resolve.LegacyResolverWrapper
 import org.jetbrains.kotlin.scripting.resolve.refineScriptCompilationConfiguration
-import java.util.concurrent.ConcurrentLinkedQueue
+import java.util.*
 import kotlin.script.experimental.dependencies.AsyncDependenciesResolver
 
-// TODO: rename and provide alias for compatibility - this is not only about dependencies anymore
 class FromRefinedConfigurationLoader internal constructor(
     private val project: Project,
     private val manager: ScriptConfigurationManagerImpl,
@@ -66,89 +65,182 @@ class FromRefinedConfigurationLoader internal constructor(
         )
     }
 
+    /**
+     * Sequentially loads script dependencies in background requested by [scheduleAsync].
+     * Progress indicator will be shown
+     *
+     * States:
+     *                                 silentWorker     underProgressWorker
+     * - sleep
+     * - silent                             x
+     * - silent and under progress          x                 x
+     * - under progress                                       x
+     */
     private inner class BackgroundLoader {
-        private var task: Task? = null
+        private var batchMaxSize = 0
+        private val work = Any()
+        private val queue: Queue<KtFile> = HashSetQueue()
+
+        private var silentWorker: SilentWorker? = null
+        private var underProgressWorker: UnderProgressWorker? = null
+        private val longRunningAlarm = Alarm(Alarm.ThreadToUse.POOLED_THREAD, project)
+        private var longRunningAlaramRequested = false
+
+        private var inTransaction: Boolean = false
 
         @Synchronized
         fun scheduleAsync(file: KtFile) {
-            if (task == null) {
-                startBatch(file)
+            if (file in queue) return
+
+            debug(file) { "added to update queue" }
+
+            queue.add(file)
+
+            batchMaxSize = maxOf(batchMaxSize, queue.size)
+            updateProgress(file)
+
+            // If the queue is longer than 3, show progress and cancel button
+            if (queue.size > 3) {
+                requireUnderProgressWorker()
             } else {
-                task!!.addTask(file)
+                requireSilentWorker()
+
+                if (!longRunningAlaramRequested) {
+                    longRunningAlaramRequested = true
+                    longRunningAlarm.addRequest(
+                        {
+                            longRunningAlaramRequested = false
+                            requireUnderProgressWorker()
+                        },
+                        1000
+                    )
+                }
             }
         }
 
         @Synchronized
-        private fun startBatch(file: KtFile) {
+        private fun requireUnderProgressWorker() {
+            if (queue.isEmpty() && silentWorker == null) return
+
+            silentWorker?.stopGracefully()
+            if (underProgressWorker == null) {
+                underProgressWorker = UnderProgressWorker().also { it.start() }
+            }
+        }
+
+        @Synchronized
+        private fun requireSilentWorker() {
+            if (silentWorker == null && underProgressWorker == null) {
+                silentWorker = SilentWorker().also { it.start() }
+            }
+        }
+
+        @Synchronized
+        fun updateProgress(next: KtFile?) {
+            underProgressWorker?.let {
+                if (next != null) it.progressIndicator.text2 = next.virtualFilePath
+                if (queue.size == 0) {
+                    // last file
+                    it.progressIndicator.isIndeterminate = true
+                } else {
+                    it.progressIndicator.isIndeterminate = false
+                    val total = batchMaxSize.toDouble() + 1
+                    val remaining = queue.size.toDouble()
+                    it.progressIndicator.fraction = 1 - remaining / total + 1 / total
+                }
+            }
+        }
+
+        @Synchronized
+        private fun ensureInTransaction() {
+            if (inTransaction) return
+            inTransaction = true
             rootsManager.startTransaction()
-            task = Task()
-            task!!.addTask(file)
-            task!!.start()
         }
 
         @Synchronized
         private fun endBatch() {
-            task = null
+            check(inTransaction)
             rootsManager.commit()
+            inTransaction = false
         }
 
-        private inner class Task {
-            private val sequenceOfFiles: ConcurrentLinkedQueue<KtFile> = ConcurrentLinkedQueue()
-            private var forceStop: Boolean = false
-            private var startedSilently: Boolean = false
+        private abstract inner class Worker {
+            private var shouldStop = false
 
-            fun start() {
-                if (KotlinScriptingSettings.getInstance(project).isAutoReloadEnabled) {
-                    startWithProgress()
-                } else {
-                    startSilently()
+            open fun start() {
+                ensureInTransaction()
+            }
+
+            fun stopGracefully() {
+                shouldStop = true
+            }
+
+            protected open fun checkCancelled() = false
+            protected abstract fun close()
+
+            protected fun run() {
+                try {
+                    while (true) {
+                        // prevent parallel work in both silent and under progress
+                        synchronized(work) {
+                            val next = synchronized(this@BackgroundLoader) {
+                                if (shouldStop) return
+
+                                if (checkCancelled() || queue.isEmpty()) {
+                                    endBatch()
+                                    return
+                                }
+
+                                queue.poll().also {
+                                    updateProgress(it)
+                                }
+                            }
+
+                            runDependenciesUpdate(next)
+                        }
+                    }
+                } finally {
+                    close()
                 }
             }
+        }
 
-            private fun restartWithProgress() {
-                forceStop = true
-                startWithProgress()
-                forceStop = false
-            }
+        private inner class UnderProgressWorker : Worker() {
+            lateinit var progressIndicator: ProgressIndicator
 
-            private fun startSilently() {
-                startedSilently = true
-                BackgroundTaskUtil.executeOnPooledThread(project, Runnable {
-                    loadDependencies(EmptyProgressIndicator())
-                })
-            }
+            override fun start() {
+                super.start()
 
-            private fun startWithProgress() {
-                startedSilently = false
-                object : com.intellij.openapi.progress.Task.Backgroundable(project, "Kotlin: Loading script dependencies...", true) {
+                object : Task.Backgroundable(project, "Kotlin: Loading script dependencies...", true) {
                     override fun run(indicator: ProgressIndicator) {
-                        loadDependencies(indicator)
+                        progressIndicator = indicator
+                        run()
                     }
                 }.queue()
             }
 
-            fun addTask(file: KtFile) {
-                if (file in sequenceOfFiles) return
+            override fun checkCancelled(): Boolean = progressIndicator.isCanceled
 
-                debug(file) { "added to update queue" }
-
-                sequenceOfFiles.add(file)
-
-                // If the queue is longer than 3, show progress and cancel button
-                if (sequenceOfFiles.size > 3 && startedSilently) {
-                    restartWithProgress()
+            override fun close() {
+                synchronized(this@BackgroundLoader) {
+                    underProgressWorker = null
                 }
             }
+        }
 
-            private fun loadDependencies(indicator: ProgressIndicator?) {
-                while (true) {
-                    if (forceStop) return
-                    if (indicator?.isCanceled == true || sequenceOfFiles.isEmpty()) {
-                        endBatch()
-                        return
-                    }
+        private inner class SilentWorker : Worker() {
+            override fun start() {
+                super.start()
 
-                    runDependenciesUpdate(sequenceOfFiles.poll())
+                BackgroundTaskUtil.executeOnPooledThread(project, Runnable {
+                    run()
+                })
+            }
+
+            override fun close() {
+                synchronized(this@BackgroundLoader) {
+                    silentWorker = null
                 }
             }
         }
