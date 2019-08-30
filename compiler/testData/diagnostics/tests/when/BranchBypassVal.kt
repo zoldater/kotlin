c1cbfe51e306cd1dcd2b92f04f4223e1cf5eb6bd
victor.petukhov <victor.petukhov@jetbrains.com>
2019-08-30 17:57:26 +0300

    1

--- compiler/testData/diagnostics/tests/when/BranchBypassVal.kt
+++ compiler/testData/diagnostics/tests/when/BranchBypassVal.kt
@@ -1,4 +1,11 @@
 // !WITH_NEW_INFERENCE
+/*
+ * RELEVANT SPEC SENTENCES (spec version: 0.1-152, test type: negative):
+ *  - expressions, when-expression -> paragraph 9 -> sentence 2
+ *  - expressions, when-expression -> paragraph 5 -> sentence 1
+ *  - expressions, when-expression -> paragraph 6 -> sentence 5
+ *  - annotations, annotation-targets -> paragraph 1 -> sentence 1
+ */
 class A
 
 fun test(a: Any): String {
